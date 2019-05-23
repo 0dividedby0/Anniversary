da02a720bd734f46544e8a1f8e6ecbe64e18f4b3
0dividedby0 <jason.halcomb@wsu.edu>
2019-05-22 19:12:58 -0600

    Initial Commit

---
+++ Anniversary/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Anniversary
+//
+//  Created by Jason Halcomb on 5/22/19.
+//  Copyright © 2019 jhalcomb. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
